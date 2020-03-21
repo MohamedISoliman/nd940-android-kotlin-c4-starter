60eaf547ee08f5f388b3eff60a75852105019225
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2020-03-22 02:20:07 +0300

    implement sign in ui design

---
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationViewModel.kt
@@ -0,0 +1,10 @@
+package com.udacity.project4.authentication
+
+import androidx.lifecycle.ViewModel
+
+/**
+ *
+ * Created by Mohamed Ibrahim on 3/22/20.
+ */
+class AuthenticationViewModel : ViewModel() {
+}
